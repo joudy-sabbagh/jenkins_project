ac5160b3403fccc94e97451268f07b3b5b273df4
joudy-sb <joudy.f.sabbagh@gmail.com>
2024-11-17 15:13:58 +0300

    Fix linting issues in app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,9 @@
 import unittest
-
 from app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from FirstName LastName!")
+        self.assertEqual(greet("World"), "Hello, World from Joudy Sabbagh!")
 
 if __name__ == "__main__":
     unittest.main()
